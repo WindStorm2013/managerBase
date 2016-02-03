728f320863b499d6db621c1b44983089c6368c7f
WindStorm <165189126@qq.com>
2016-02-03 21:11:05 +0800

    目录提交

---
+++ base-framework/src/main/java/com/windstorm/base/framework/App.java
@@ -0,0 +1,13 @@
+package com.windstorm.base.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
